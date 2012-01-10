76fcd8e194ca40ab07da003283631641650fd6f4
Nicholas Watson <nwwatson@gmail.com>
2012-01-09 23:34:42 -0500

    updated to latest version of pushpop

--- pushpop.js
+++ pushpop.js
@@ -1,30 +1,61 @@
 'use strict';
 
+// Polyfill for requestAnimationFrame() / cancelAnimationFrame()
+(function() {
+  var lastTime = 0;
+  var vendors = ['webkit', 'moz', 'ms', 'o'];
+  
+  for (var i = 0; i < vendors.length && !window.requestAnimationFrame; i++) {
+    window.requestAnimationFrame = window[vendors[i] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[i] + 'CancelAnimationFrame'] ||
+      window[vendors[i] + 'RequestCancelAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function(callback, element) {
+      var currTime = Date.now();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      
+      lastTime = currTime + timeToCall;
+      
+      return id;
+    };
+  }
+
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function(id) {
+      window.clearTimeout(id);
+    };
+  }
+})();
+
 var Pushpop = {
   views: [],
+  $topbar: null,
+  $backButton: null,
+  
   handleEvent: function(evt) {
     switch (evt.type) {
       case 'webkitTransitionEnd':
       case 'oTransitionEnd':
       case 'transitionend':
-        var activeView = Pushpop.activeView();
-        var $activeViewElement = $('.view.active');
-        var $newActiveViewElement = $(activeView.element);
+        $(document.body).removeClass('transition');
         
-        if (!Pushpop.isGecko()) {
-          $(document.documentElement).removeClass('transition');
-        }
+        window.requestAnimationFrame(function() {
+          var activeView = Pushpop.activeView();        
+          var $activeViewElement = $('.view.active');
+          var $newActiveViewElement = $(activeView.element);
         
-        $activeViewElement.removeClass('active push pop transition ' +
-          'slideHorizontal slideVertical flipHorizontal flipVertical cardSwap crossFade zoom');
-        $newActiveViewElement.removeClass('push pop transition ' +
-          'slideHorizontal slideVertical flipHorizontal flipVertical cardSwap crossFade zoom');
-        $newActiveViewElement.addClass('active')
-          .unbind('webkitTransitionEnd').unbind('oTransitionEnd').unbind('transitionend');
-        
-        setTimeout(function() {
-          window.scrollTo(activeView.scrollX, activeView.scrollY);
-        }, 100);
+          $activeViewElement.removeClass('active').css('top', '').unwrap();
+          $newActiveViewElement.addClass('active').css('top', '').unwrap();
+          
+          window.requestAnimationFrame(function() {
+            window.scrollTo(activeView.scrollX, activeView.scrollY);
+          });
+        });
         break;
       default:
         break;
@@ -50,7 +81,7 @@ var Pushpop = {
     var incrementX = (x - window.pageXOffset) / stepCount;
     var incrementY = (y - window.pageYOffset) / stepCount;
     
-    var interval = setInterval(function() {
+    var interval = window.setInterval(function() {
       var currentX = window.pageXOffset + incrementX;
       var currentY = window.pageYOffset + incrementY;
       
@@ -67,10 +98,10 @@ var Pushpop = {
       }
       
       if (currentX === x && currentY === y) {
-        clearInterval(interval);
+        window.clearInterval(interval);
         
         if (callback && typeof callback === 'function') {
-          setTimeout(function() {
+          window.setTimeout(function() {
             callback();
           }, 100);
         }
@@ -114,12 +145,16 @@ var Pushpop = {
   push: function(view, transition) {
     var views = this.views;
     var isGecko = this.isGecko();
+    var $backButton = this.$backButton;
     var transition, activeView, newActiveView;
     
     view.isRoot = views.length === 0;
     
     if (!view.isRoot) {
+      $(document.body).addClass('transition');
+      $backButton.addClass('active');
       activeView = this.activeView();
+      activeView.lastTransition = transition;
       newActiveView = view;
     }
     
@@ -147,42 +182,38 @@ var Pushpop = {
       }
     }
     
-    $newActiveViewElement.bind('webkitTransitionEnd oTransitionEnd transitionend', this.handleEvent);
+    var activeOffsetY = 0 - activeView.scrollY;
+
+    var $activeTransitionContainer = $('<div class="transition-container push ' + transition + ' active"/>');
+    var $newActiveTransitionContainer = $('<div class="transition-container push ' + transition + '"/>');
     
-    setTimeout(function() {
-      window.scrollTo(0, 0);
+    $activeViewElement.wrap($activeTransitionContainer).css('top', activeOffsetY + 'px');
+    $newActiveViewElement.wrap($newActiveTransitionContainer);
 
-      $activeViewElement.addClass(transition + ' push');
-      $newActiveViewElement.addClass(transition + ' push');
+    $activeTransitionContainer = $activeViewElement.parent();
+    $newActiveTransitionContainer = $newActiveViewElement.parent();
+    $newActiveTransitionContainer.bind('webkitTransitionEnd oTransitionEnd transitionend', this.handleEvent);
 
-      setTimeout(function() {
-        if (!isGecko) {
-          $(document.documentElement).addClass('transition');
-        }
-
-        $activeViewElement.addClass('transition');
-        $newActiveViewElement.addClass('transition');
-      }, isGecko ? 100 : 0);
-    }, 100);
+    window.requestAnimationFrame(function() {
+      $activeTransitionContainer.addClass('transition');
+      $newActiveTransitionContainer.addClass('transition');
+    });
   },
   pop: function(viewOrTransition, transition) {
     var views = this.views;
     var isGecko = this.isGecko();
+    var $backButton = this.$backButton;
     var transition, activeView, newActiveView;
     
     if (views.length <= 1) return;
     
-    window.scrollTo(0, 0);
+    $(document.body).addClass('transition');
     
     activeView = this.activeView();
     
-    if (typeof viewOrTransition === 'string') {
-      transition = viewOrTransition;
-    } else {
+    if (viewOrTransition && typeof viewOrTransition !== 'string') {
       newActiveView = viewOrTransition;
-    }
-  
-    if (newActiveView) {
+      
       var isOnStack = this.getView(newActiveView) !== null;
   
       if (isOnStack) {
@@ -190,11 +221,21 @@ var Pushpop = {
           views.pop();
         }
       }
-    }
-  
-    else {
+    } else {
+      if (viewOrTransition) {
+        transition = viewOrTransition;
+      }
+      
       views.pop();
       newActiveView = this.activeView();
+    }
+    
+    if (!transition) {
+      transition = newActiveView.lastTransition;
+    }
+    
+    if (views.length === 1) {
+      $backButton.removeClass('active');
     }
     
     var $activeViewElement = $(activeView.element);
@@ -214,19 +255,23 @@ var Pushpop = {
       }
     }
     
-    $activeViewElement.addClass(transition + ' pop');
-    $newActiveViewElement.addClass(transition + ' pop');
-    
-    $newActiveViewElement.bind('webkitTransitionEnd oTransitionEnd transitionend', this.handleEvent);
-    
-    setTimeout(function() {
-      if (!isGecko) {
-        $(document.documentElement).addClass('transition');
-      }
-      
-      $activeViewElement.addClass('transition');
-      $newActiveViewElement.addClass('transition');
-    }, isGecko ? 100 : 0);
+    var activeOffsetY = 0 - activeView.scrollY;
+    var newActiveOffsetY = 0 - newActiveView.scrollY;
+
+    var $activeTransitionContainer = $('<div class="transition-container pop ' + transition + ' active"/>');
+    var $newActiveTransitionContainer = $('<div class="transition-container pop ' + transition + '"/>');
+
+    $activeViewElement.wrap($activeTransitionContainer).css('top', activeOffsetY + 'px');
+    $newActiveViewElement.wrap($newActiveTransitionContainer).css('top', newActiveOffsetY + 'px');
+
+    $activeTransitionContainer = $activeViewElement.parent();
+    $newActiveTransitionContainer = $newActiveViewElement.parent();
+    $newActiveTransitionContainer.bind('webkitTransitionEnd oTransitionEnd transitionend', this.handleEvent);
+
+    window.requestAnimationFrame(function() {
+      $activeTransitionContainer.addClass('transition');
+      $newActiveTransitionContainer.addClass('transition');
+    });
   }
 };
 
@@ -275,19 +320,102 @@ Pushpop.View.prototype = {
   isRoot: false,
   href: '',
   scrollX: 0,
-  scrollY: 0
+  scrollY: 0,
+  lastTransition: 'slideHorizontal'
+};
+
+Pushpop.TableView = function(element) {
+  var $element = $(element);
+  var $body = $(document.body);
+  
+  if ($element.size() === 1) {
+    $element.data('tableview', this);
+    
+    // Required to enable :active pseudo-state to occur.
+    $element.bind('touchstart', function() {});
+    
+    // Set up picker cells.
+    $element.children('li.picker-cell').each(function(index, element) {
+      var $element = $(element);
+      var $pickerTableView = $element.children('ul.tableview:first');
+      
+      if ($pickerTableView.size() !== 1) return;
+      
+      var $input = $element.children('input[type="hidden"]:first');
+      var value = $input.val();
+      
+      var $selectedCell = $pickerTableView.children('li[data-value="' + value + '"]:first');
+      
+      if ($selectedCell.size() === 0) {
+        $selectedCell = $pickerTableView.children('li:contains("' + value + '")');
+      }
+      
+      $selectedCell.addClass('checkmark');
+      
+      var text = ($selectedCell.size() > 0) ? $selectedCell.text() : '(None)';
+      var $span = $('<span>' + text + '</span>');
+      
+      $element.append($span).append($input);
+      
+      var $view = $('<div class="view"/>').append($pickerTableView);
+      
+      $body.append($view);
+      
+      $pickerTableView.delegate('li', 'click', function(evt) {
+        var $this = $(this);
+        
+        if ($this.hasClass('header')) return;
+        
+        var text = $this.text();
+        var value = $this.attr('data-value') || text;
+        
+        $pickerTableView.find('li.checkmark').removeClass('checkmark');
+        $this.addClass('checkmark');
+      
+        $span.html(text);
+        $input.val(value);
+        
+        Pushpop.pop();
+      });
+      
+      $element.data('picker-view', new Pushpop.View($view));
+      $element.data('picker-selection', $span.get(0));
+    });
+    
+    $element.delegate('li.picker-cell', 'click', function(evt) {
+      var $this = $(this);
+      var pickerView = $this.data('picker-view');
+      
+      Pushpop.push(pickerView, 'slideHorizontal');
+    });
+    
+    this.element = $element.get(0);
+  }
+};
+
+Pushpop.TableView.prototype = {
+  element: null
 };
 
 $(function() {
+  
+  var $topbar = Pushpop.$topbar = $('.topbar');
+  var $backButton = Pushpop.$backButton = $('<a class="btn pop pull-left" href="#">Back</a>');
+  $topbar.find('.container').prepend($backButton);
   
   // Set the first <div class="view"/> child of the document body as the root View.
   Pushpop.push(new Pushpop.View($('body > div.view:first').addClass('active')));
   
   // Attach all Views as immediate children of the document body.
-  var body = $(document.body);
+  var $body = $(document.body);
   
   $('.view').each(function(index, element) {
-    body.append(element);
+    $body.append(element);
+  });
+  
+  // Initialize all TableViews.
+  $('.tableview').each(function(index, element) {
+    var tableView = new Pushpop.TableView(element);
   });
   
   // Attach a click event handler to all anchor links set up for push or pop.
@@ -325,21 +453,19 @@ $(function() {
       transition = 'zoom';
     }
     
-    if (transition) {
-      if (isPop && href === '#') {
-        Pushpop.pop(transition);
+    if (isPop && href === '#') {
+      Pushpop.pop(transition);
+    }
+
+    else {
+      var view = Pushpop.getView(href) || new Pushpop.View(href);
+  
+      if (isPop) {
+        Pushpop.pop(view, transition);
       }
   
       else {
-        var view = Pushpop.getView(href) || new Pushpop.View(href);
-    
-        if (isPop) {
-          Pushpop.pop(view, transition);
-        }
-    
-        else {
-          Pushpop.push(view, transition);
-        }
+        Pushpop.push(view, transition);
       }
     }
     
