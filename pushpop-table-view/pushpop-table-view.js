d74757013b9f20ab6af7d50983d2d9ce4dc3b93f
Justin D'Arcangelo <justindarc@gmail.com>
2012-07-18 14:15:30 -0400

    Fixed focus issue with InlineTextInputTableViewCell.

--- pushpop-table-view/pushpop-table-view.js
+++ pushpop-table-view/pushpop-table-view.js
@@ -1758,6 +1758,7 @@ Pushpop.InlineTextInputTableViewCell.prototype.getHtml = function() {
 
 Pushpop.InlineTextInputTableViewCell.prototype.didReceiveTap = function() {
   var $element = this.$element;
+  $element.children('input').trigger('focus');
   window.setTimeout(function() { $element.removeClass('pp-table-view-selected-state'); }, 100);
 };
 
